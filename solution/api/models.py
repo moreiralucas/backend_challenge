bb7cedf9d2271617edb7228d6dc06b4be28e8157
Lucas Moreira <lucas-mopi@hotmail.com>
2021-06-06 22:20:08 -0300

    feature: Implements Car and Tyre models

--- solution/api/models.py
+++ solution/api/models.py
@@ -1,3 +1,29 @@
+import logging
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from carmanager.settings import (
+    TYRE_NEED_MAINTENANCE,
+    CAR_GAS_CAPACITY
+)
 
-# Create your models here.
+
+class Tyre(models.Model):
+    degradation = models.PositiveSmallIntegerField(
+        default=0
+    )
+    car = models.ForeignKey(
+        'api.Car',
+        models.CASCADE,
+        related_name='tyre'
+    )
+
+
+class Car(models.Model):
+    gas_capacity = models.PositiveSmallIntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(1000)],
+    )
+    gas = models.PositiveSmallIntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(1000)],
+    )
