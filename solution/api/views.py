82357f0eb1fee77aebaa7ae42699debae7cae07a
Lucas Moreira <lucas-mopi@hotmail.com>
2021-06-07 00:41:51 -0300

    feature: Implements View set

--- solution/api/views.py
+++ solution/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CarSerializer, TyreSerializer
+from .models import Car, Tyre
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+    def create(self, _):
+        car = Car.createCar()
+        serializer = self.get_serializer(car)
+        return Response(serializer.data)
+
+
+class TyreViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tyre.objects.all()
+    serializer_class = TyreSerializer
