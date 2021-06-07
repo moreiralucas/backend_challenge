e489c82762d5773fd3ac6ab9a7661aae2ea1f833
Lucas Moreira <lucas-mopi@hotmail.com>
2021-06-07 00:48:06 -0300

    feature; Create test api file

---
+++ solution/api/tests_api/test_api.py
@@ -0,0 +1,4 @@
+from api.tests_api.base_test import BaseTest
+
+class APITest(BaseTest):
+    pass
--- solution/api/urls.py
+++ solution/api/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework.authtoken.views import obtain_auth_token
 from api.views import (CarViewSet, TyreViewSet)
 
 
